3fdab6129329eaeb4c2195251cf2f8d8ed4e174c
sakakibara akane <sakaki839@gmail.com>
2025-03-25 22:03:10 +0900

    リモートリポジトリの変更

--- src/resources/views/register.php
+++ src/resources/views/register.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="ja">
+< lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="register.css">
 </head>
-<body>
-    あ
+<>
+        <header>
+            header<br>
+        </header>
+        <div class="flexbox-container">
+            <aside>
+                    aside<br>
+            </aside>
+            <main>
+                main<br>
+            </main>
+            <aside>
+                aside<br>
+            </aside>
+        </div>
+        <footer>
+            footer<br>
+        </footer>
 </body>
 </html>
