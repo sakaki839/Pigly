b53b16f28cb7b08f5fe64fb2f1e5311d8233c568
sakakibara akane <sakaki839@gmail.com>
2025-03-30 00:06:30 +0900

    リモートリポジトリの変更

--- src/resources/views/register.php
+++ src/resources/views/register.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-< lang="ja">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="register.css">
+    
+   <link rel="stylesheet" href="css/register.css">
 </head>
-<>
+ 
+<body>
         <header>
             header<br>
         </header>
@@ -15,7 +17,74 @@
                     aside<br>
             </aside>
             <main>
-                main<br>
+                 <div class="contact-form__content">
+      <div class="contact-form__heading">
+
+      <h1>PiGly</h1>
+        <h3>新規会員登録</h3>
+        <div>STEP1 アカウント情報の登録</div>
+      </div>
+
+    
+      <form class="form">
+
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">お名前</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="text" name="name" placeholder="お名前を入力" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">メールアドレス</span> 
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="email" name="email" placeholder="メールアドレスを入力" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">パスワード</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="tel" name="tel" placeholder="パスワードを入力" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+
+
+          </div>
+        </div>
+
+    <div class="buttons">
+
+
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">次に進む</button>
+        </div>
+
+        <div class=rog>
+            <a href="">ログインはこちら</a>
+        </div>
+    </div>
+
+
+      </form>
+    </div>
             </main>
             <aside>
                 aside<br>
@@ -24,5 +93,5 @@
         <footer>
             footer<br>
         </footer>
-</body>
+    </body>
 </html>
